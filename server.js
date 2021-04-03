9654cd375e4a23ef7d2fbb7ca028d1f547500053
bv-loop <72999113+bv-loop@users.noreply.github.com>
2021-04-03 17:03:42 +0100

    Change route

--- server.js
+++ server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: false}));
 /////////////////////////////////////////////////////////////
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
-      })
 }
 
 
